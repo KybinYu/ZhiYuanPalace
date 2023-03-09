366ea349fa18f7554aff9c4b80b0340cc9a675bb
Kybin <mebiustt@outlook.com>
2023-03-09 17:14:16 +0800

    add animation

--- src/views/CommonLayOut/HeaderBar/components/PageSwitching.vue
+++ src/views/CommonLayOut/HeaderBar/components/PageSwitching.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page-switching">
-    <button id="switch-up-btn" @click="pageSwitching(-1)">
+    <button id="switch-up-btn" @click="pageSwitching(-1), animation($event)">
       <i class="segoe-fluent-icons">&#xe973;</i>
     </button>
-    <button id="switch-down-btn" @click="pageSwitching(1)">
+    <button id="switch-down-btn" @click="pageSwitching(1), animation($event)">
       <i class="segoe-fluent-icons">&#xe974;</i>
     </button>
   </div>
@@ -11,6 +11,15 @@
 <script>
 export default {
   methods: {
+    animation(e) {
+      //延迟动画
+      //获得点击元素的第一个子元素
+      const icon = e.currentTarget.firstElementChild
+      icon.classList.add('switch-page-icon-animation')
+      setTimeout(function () {
+        icon.classList.remove('switch-page-icon-animation')
+      }, 200)
+    },
     pageSwitching(num) {
       if (num == 1) {
         this.$router.go(1)
@@ -36,6 +45,15 @@ export default {
     vertical-align: super;
     padding: 0;
     font-size: 14px;
+
+    i.segoe-fluent-icons {
+      display: inline-block;
+      transform: none;
+      transition: all 400ms;
+      &.switch-page-icon-animation {
+        transform: scale(2, 0.5);
+      }
+    }
 
     &:hover {
       color: var(--td-brand-color);
